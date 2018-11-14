6a69e0b9fc9ecf69f43c437a089907ef0b9bca99
Eduardo <spoky.br@gmail.com>
2018-11-14 09:10:23 -0200

    improvements

--- Xamarin.ConsumingAPI/Xamarin.ConsumingAPI/App.xaml.cs
+++ Xamarin.ConsumingAPI/Xamarin.ConsumingAPI/App.xaml.cs
@@ -5,64 +5,60 @@ using System.Net;
 using System.Net.Http;
 using Xamarin.ConsumingAPI.Model;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace Xamarin.ConsumingAPI
 {
     public partial class App : Application
-	{
+    {
         private static HttpClient client = new HttpClient();
+        private Label labelTitle = new Label();
+        private Button botaoPesquisa = new Button();
         private Entry textBusca = new Entry();
         private ListView lista = new ListView();
+        private ContentPage pagina = new ContentPage();
 
-        public App ()
-		{
-			InitializeComponent();
+        public App()
+        {
+            InitializeComponent();
 
-            //MainPage = new Xamarin.ConsumingAPI.MainPage();
+            labelTitle.Text = "Star Wars Ships";
+            labelTitle.VerticalTextAlignment = TextAlignment.Center;
+            labelTitle.FontSize = 28;
 
-            textBusca = new Entry();
-            textBusca.Placeholder = "Informe o valor para a pesquisa";
-
-            var botaoPesquisa = new Button();
             botaoPesquisa.Text = "Pesquisar";
             botaoPesquisa.Clicked += BotaoPesquisa_Clicked;
 
-            lista = new ListView();
+            textBusca.Placeholder = "Informe o valor para a pesquisa";
 
             var layout = new StackLayout();
             layout.Padding = 20;
             layout.Spacing = 20;
+            layout.Children.Add(labelTitle);
             layout.Children.Add(textBusca);
             layout.Children.Add(botaoPesquisa);
             layout.Children.Add(lista);
 
-            var pagina = new ContentPage();
             pagina.Content = layout;
 
             MainPage = pagina;
-		}
+        }
 
         private async void BotaoPesquisa_Clicked(object sender, EventArgs e)
         {
-            var url = "https://swapi.co/api/starships/?search=" + WebUtility.UrlEncode(textBusca.Text) + "&format=json";
-            var response = await client.GetStringAsync(url);
-            var items = JsonConvert.DeserializeObject<ApiResult>(response);
-            lista.ItemsSource = items?.Results.Select(item => $"{item.Name}/{item.Starship_class}/{item.Manufacturer}");
+            try
+            {
+                var url = "https://swapi.co/api/starships/?search=" + WebUtility.UrlEncode(textBusca.Text) + "&format=json";
+                var response = await client.GetStringAsync(url);
+                var apiResult = JsonConvert.DeserializeObject<ApiResult>(response);
+                var items = apiResult?.Results.Select(item => $"{item.Name}/{item.Manufacturer}/{item.Starship_class}").ToList();
+                lista.ItemsSource = items;
+            }
+            catch (Exception ex)
+            {
+                await pagina.DisplayAlert("Erro", ex.Message, "OK");
+            }
         }
-
-        protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
-
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
-
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+    }
 }
--- Xamarin.ConsumingAPI/Xamarin.ConsumingAPI/Model/Nave.cs
+++ Xamarin.ConsumingAPI/Xamarin.ConsumingAPI/Model/Nave.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Xamarin.ConsumingAPI.Model
+﻿namespace Xamarin.ConsumingAPI.Model
 {
     public class Nave
     {
